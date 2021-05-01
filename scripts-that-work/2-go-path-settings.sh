bab7d9980440aa50d03a56aa83de240022e0c953
patogit <akiktakat@protonmail.com>
2021-05-01 12:59:50 -0500

    update, remove GOROOT

--- scripts-that-work/2-go-path-settings.sh
+++ scripts-that-work/2-go-path-settings.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 # ./2-go-path-settings.sh go-workspace
 
-GODIRECTORY=$1
-
-export GOROOT=/usr/lib/go
-export GOPATH=${HOME}/$GODIRECTORY
-export GOBIN=$GOPATH/bin
-export PATH=${PATH}:${GOROOT}/bin:${GOBIN}
+echo 'export GOPATH="$HOME/go"' >> ~/.profile
+echo 'export GOBIN="$GOPATH/bin"' >> ~/.profile
+echo 'export PATH="$GOBIN:$PATH"' >> ~/.profile
 
 echo "Now run . ~/.profile (or . ~/.bashrc if the first didn't work)"
